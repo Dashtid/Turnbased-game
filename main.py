4141793649a21ec3799f33e998300cb05b242dea
Dashtid <47575784+Dashtid@users.noreply.github.com>
2023-08-07 19:55:53 +0000

    Added the early stages of event happening to characters

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # # Contents in this file are executed when we hit “Run”
+from enum import Enum
+import random as rand
 import work as work
+
+class AttackType(Enum):
+  NORMAL = 25
+  HEAVY = 50
 
 def end_condition(character):
   # Checking if the character is still alive
@@ -8,6 +14,21 @@ def end_condition(character):
   return False
 
 
+  ## Task 2
+  ## Either find a chest and get a piece of armor OR become attacked
+  ## Some % chance for a chest or attack
+  ## If it's a chest what does it have?
+  ## If it's an attack which attack
+
+def event(character):
+  random_number = rand.random()
+  if random_number >= 0.5:
+    work.attack(character, 'Torso', AttackType.NORMAL)
+    return None
+  else:
+    return None # Find a chest with an item?
+    
+    
 # Just some hardcorded booleans for now.
 ongoing_game = True
 character = None
@@ -16,13 +37,7 @@ character = None
 while ongoing_game:
   if character is None:
     work.character_creation() # Creating a character if none exists
-    
-  ## Task 2
-  ## Either find a chest and get a piece of armor OR become attacked
-  ## Some % chance for a chest or attack
-  ## If it's a chest what does it have?
-  ## If it's an attack which attack
-  
+  event(character) # Think this is not quite right
   if end_condition:
     print("You have died")
   
