ef2675945bb62cf41f69f5739b64df01b03c1675
Dashtid <47575784+Dashtid@users.noreply.github.com>
2023-08-14 20:49:22 +0000

    Added docstring to attack and created attack_message function

--- Work/game/combat.py
+++ Work/game/combat.py
@@ -7,32 +7,43 @@ from utils import bodypart_list_to_dict
 # Should probably be a part of character!
 
 def attack(victim, bodypart, attack_type):
+  """
+  Performs an attack, of a certain strenght on a victims bodyparts, in three stages.
+  1. Prepares the attack by gathering parameters from the victim.
+  2. Executes the attack and takes into account protections that may apply.
+  3. Updates the player of outcome of the attack.
+
+  Example:
+  attack('victim', 'Neck', 'Weak') -> (
+  'name': 'victim_name',
+    'body': [
+      {'name': 'Head',  'health': 10}, 
+      {'name':  'Neck', 'health': 10}, 
+      {'name':  ...,    'health': ...},
+    ],
+    'health': 190,
+    'armor' : [],
+  }, 10)
+  """
+  
   # -- Preparing the attack -- #
   # Converts the bodypart list into a dictionary
   victim_body = bodypart_list_to_dict(victim['body'])
-  # Fetches bodypart name from the dictionary 
   bodypart_name = victim_body[bodypart]['name']
-  # Fetches the current health of the bodypart, before the attack
   bodypart_health = victim_body[bodypart]['health']
-  # Fetches the protections that apply to that bodypart
+  # Calculates protections that apply to that bodypart
   protection = calculate_protection(victim, bodypart)
   
   # -- Performing the attack -- #
-  # Changes protections value to a multiplier of the attack and then applies the attack
   new_bodypart_health = bodypart_health - attack_type * (protection / 100) 
-  # Updates the victims bodypart with the new health value after the attack
   victim_body[bodypart]['health'] = new_bodypart_health 
 
   # -- Updating user on the status of the character -- #
-  # Calculate the damage taken to the bodypart
   victim_damage = bodypart_health - new_bodypart_health
-  # Calculate the new overall health of the character
   new_health = character.calculate_health(victim) 
-
-  # TODO: Normally we do not print within functions, we create a function for it
-  print(f'Damage taken is: {victim_damage}!')
-  print(f'New health of {bodypart_name} is: {new_bodypart_health} hp')
-  print(f"New health of {victim['name']} is: {new_health} hp")
+  
+  # Unsure as of how to do this :P
+  attack_message(victim, victim_damage, bodypart_name, new_bodypart_health, new_health)
   return victim, victim_damage
 
 def attack_type(type_chosen):
@@ -58,3 +69,10 @@ def calculate_protection(character, bodypart):
   # Ensures that totalt_protection cannot be higher than 99%
   total_protection = min(total_protection, 99) 
   return total_protection
+
+# Dunno...
+def attack_message(victim, victim_damage, bodypart_name, new_bodypart_health, new_health):
+  status = f'''Damage taken is: {victim_damage}!
+  New health of {bodypart_name} is: {new_bodypart_health} hp
+  New health of {victim['name']} is: {new_health} hp'''
+  return status
