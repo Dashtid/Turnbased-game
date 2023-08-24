3af98739ea1701dc84bb456e59fd4488de13fba7
Dashtid <47575784+Dashtid@users.noreply.github.com>
2023-08-24 18:44:31 +0000

    added Protection class

--- Work/game_oop/armor.py
+++ Work/game_oop/armor.py
@@ -2,11 +2,15 @@
 class Armor():
   """
   """
-  def __init__(self, name, protections = []):
+  def __init__(self, name, protections):
     self.name = name
     self.protections = protections
 
 class Protection():
   """
   """
+  def init(self, body_part, percentage):
+    self.body_part = body_part
+    self.percentage = percentage
+    
     
